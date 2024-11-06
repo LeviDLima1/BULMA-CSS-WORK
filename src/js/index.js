53df730bb0c431d88e0df6c09abe3619ef30cbae
Levi Duarte <duartelevi1@gmail.com>
2024-11-05 21:27:40 -0300

    adicionando JS

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+activeButton1 = document.getElementById("activeButton1")
+activeButton2 = document.getElementById("activeButton2")
+activeButton3 = document.getElementById("activeButton3")
+activeButton4 = document.getElementById("activeButton4")
+activeButton5 = document.getElementById("activeButton5")
+activeButton6 = document.getElementById("activeButton6")
+
+modal1 = document.getElementById("modal1")
+
+openModal = document.getElementsByClassName("openModal")
+
+modal1
+
+
